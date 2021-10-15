85fdb7c174153c0efa47d1a9810ca3fb8b606c9a
Naseus <tdevries4i@gmail.com>
2021-10-15 17:31:30 -0400

    fix: missing secret on deployment

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['DJANGO']['SECRET']
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    SECRET_KEY = config['DJANGO']['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=1))
